6307a1336dfcc11c92f167898ef142e53403c1f0
Kai Page <kaibug@gmail.com>
2019-02-10 10:38:25 -0600

    Switched line endings from CR to LF
    I think, at least. Doesn't Git for Windows change CRLFs to LFs when committing?

--- Robot_Lights/Robot_Lights.ino
+++ Robot_Lights/Robot_Lights.ino
@@ -1,1 +1,24 @@
-#include <FastLED.h> //#define DATA_PIN 6 not using this because each strip has it's own defined in fastled.addleds#define NUM_LEDS 64 CRGB leds[NUM_LEDS];void setup() {  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);  }void loop() {  auton();  }void auton() {  leds[0, 64] = CRGB::Blue;  FastLED.show();  delay(500);  // Now turn the LED off, then pause  leds[0] = CRGB::Black;  FastLED.show();  delay(500);}
+#include <FastLED.h>
+ 
+//#define DATA_PIN 6 not using this because each strip has it's own defined in fastled.addleds
+#define NUM_LEDS 64
+ 
+CRGB leds[NUM_LEDS];
+void setup() {
+  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);  
+}
+
+void loop() {
+  auton();  
+}
+
+void auton() {
+  leds[0, 64] = CRGB::Blue;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
