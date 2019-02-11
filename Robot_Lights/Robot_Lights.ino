fff21b1758a7e7364810809061c11fcc2f15c986
memtech3 <20732350+memtech3@users.noreply.github.com>
2019-02-10 21:04:25 -0600

    changed some stuff

--- Robot_Lights/Robot_Lights.ino
+++ Robot_Lights/Robot_Lights.ino
@@ -1,1 +1,60 @@
-#include <FastLED.h> //#define DATA_PIN 6 not using this because each strip has it's own defined in fastled.addleds#define NUM_LEDS 64 CRGB leds[NUM_LEDS];void setup() {  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);  FastLED.addLeds<NEOPIXEL,1>(leds, NUM_LEDS);  }void loop() {  auton();  }void auton() {  leds[0, 64] = CRGB::Blue;  FastLED.show();  delay(500);  // Now turn the LED off, then pause  leds[0] = CRGB::Black;  FastLED.show();  delay(500);}
+#include <FastLED.h>
+ 
+//#define DATA_PIN 6 not using this because each strip has it's own defined in fastled.addleds
+#define NUM_LEDS_Elevator 64
+int i; 
+CRGB ledsElevator[NUM_LEDS_Elevator];
+void setup() {
+  //LED Code
+  FastLED.addLeds<NEOPIXEL,1>(ledsElevator, NUM_LEDS_Elevator);
+  FastLED.addLeds<NEOPIXEL,2>(ledsElevator, NUM_LEDS_Elevator);
+  //Serial Code
+  Serial.begin(9600);
+    
+}
+
+void loop() {
+  int i = Serial.read();  
+}
+
+void switchPattern() {
+  switch (i) {
+    case 1: //switches when i is 1
+      auton(); //calls auton function
+      break;
+    case 2: //switches when i is 2
+      teleop(); //calls teleop funcion
+      break;
+    default:
+      noInstructions();
+      break;
+  }
+}
+
+void auton() {
+  ledsElevator[0, 64] = CRGB::Green;
+  FastLED.show();
+  delay(500);
+  ledsElevator[0, 64] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
+
+void teleop() {
+  ledsElevator[0, 64] = CRGB::Orange;
+  FastLED.show();
+  delay(500);
+  ledsElevator[0, 64] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
+
+void noInstructions() {
+  ledsElevator[0, 64] = CRGB::Blue;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  ledsElevator[0, 64] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
